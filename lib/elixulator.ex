fe88a6c96296d02f21e691cc91fb6b6e943ee1a4
kage-mac <kagemiku@gmail.com>
2015-12-14 19:11:50 +0900

    Implement elixulator module.

--- lib/elixulator.ex
+++ lib/elixulator.ex
@@ -1,2 +1,21 @@
 defmodule Elixulator do
+  def add(x, y) do
+    x + y
+  end
+
+  def sub(x, y) do
+    x - y
+  end
+
+  def mul(x, y) do
+    x * y
+  end
+
+  def div(_x, 0) do
+    nil
+  end
+
+  def div(x, y) do
+    x / y
+  end
 end
--- test/elixulator_test.exs
+++ test/elixulator_test.exs
@@ -2,7 +2,29 @@ defmodule ElixulatorTest do
   use ExUnit.Case
   doctest Elixulator
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "add" do
+    x = Elixulator.add(10, 20)
+    assert x == 30
   end
+
+  test "sub" do
+    x = Elixulator.sub(10, 20)
+    assert x == -10
+  end
+
+  test "mul" do
+    x = Elixulator.mul(10, 20)
+    assert x == 200
+  end
+
+  test "div with valid values" do
+    x = Elixulator.div(10, 20)
+    assert x == 0.5
+  end
+
+  test "div with invalid values" do
+    x = Elixulator.div(10, 0)
+    assert x == nil
+  end
+
 end
